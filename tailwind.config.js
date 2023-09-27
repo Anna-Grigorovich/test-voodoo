1fdaa7b0dddf3bc79aa6dfbeaa430b7c39ee5350
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-09-28 00:36:46 +0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBgColor: '#FCF7E6',
+      },
+    },
   },
   plugins: [],
 };
