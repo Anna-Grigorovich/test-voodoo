6287144ce90e0ae22ce8c83bea9eb028d7d8e152
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-09-29 01:50:43 +0300

    bug fix

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,21 +1,22 @@
 export function saveToLocalStorage(product) {
-  // Получите текущий список товаров из локального хранилища (если он существует)
   let existingProducts = JSON.parse(localStorage.getItem('cart')) || [];
 
-  // Проверьте, есть ли товар с таким же ID
-  const existingProductIndex = existingProducts.findIndex(
-    existingProduct => existingProduct.id === product.id
-  );
-  console.log(product.id);
-  console.log(existingProducts);
+  existingProducts = existingProducts.map(existingProduct => {
+    return {
+      ...existingProduct,
+      id: parseFloat(existingProduct.id),
+    };
+  });
+  product.id = parseFloat(product.id);
+  const existingProductIndex = existingProducts.findIndex(existingProduct => {
+    return existingProduct.id === product.id;
+  });
   if (existingProductIndex !== -1) {
-    // Если товар с таким ID уже существует, удалите его
-    existingProducts.splice(existingProductIndex, 1);
+    const updatedProducts = [...existingProducts];
+    updatedProducts.splice(existingProductIndex, 1);
+    localStorage.setItem('cart', JSON.stringify(updatedProducts));
+  } else {
+    existingProducts.push(product);
+    localStorage.setItem('cart', JSON.stringify(existingProducts));
   }
-
-  // Добавьте новый товар к существующему списку
-  existingProducts.push(product);
-
-  // Сохраните обновленный список товаров в локальное хранилище
-  localStorage.setItem('cart', JSON.stringify(existingProducts));
 }
