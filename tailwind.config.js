f0da1f95120a0622901c83bc8debab032ad368d9
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-09-28 05:29:31 +0300

    add html partials + add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         mainBgColor: '#FCF7E6',
       },
+      fontFamily: {
+        sans: ['Space Grotesk', 'Space Mono'],
+      },
     },
   },
   plugins: [],
