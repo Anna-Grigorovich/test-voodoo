f1251385a8e8c990fc61426a919495ac2a3b63c6
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-09-30 19:49:28 +0300

    bugs fix

--- src/js-partials/total.js
+++ src/js-partials/total.js
@@ -9,9 +9,10 @@ export async function createTotal() {
 
 export async function totalMarkup() {
   const total = await calculateTotal();
+  const roundedTotal = total.toFixed(2);
   const markup = () => {
     return ` <p class="h-3 text-yellow-50 text-sm">TOTAL</p>
-          <p class="h-3 text-yellow-50 text-sm">${total || 0}</p>`;
+          <p class="h-3 text-yellow-50 text-sm">${roundedTotal || 0} Kr</p>`;
   };
   return markup();
 }
